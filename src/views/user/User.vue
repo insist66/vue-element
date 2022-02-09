7cc70f2ea5ac867cf01d2ac9efea749ecafe017d
sbobo <sbobo@laixue365.com>
2022-02-09 17:24:10 +0800

    添加用户

--- src/views/user/User.vue
+++ src/views/user/User.vue
@@ -58,11 +58,24 @@
     </el-card>
 
     <el-dialog
-      title="提示"
+      title="添加用户"
       :visible.sync="addDialogVisible"
       width="50%"
       :before-close="handleClose">
-      <span>这是一段信息</span>
+      <el-form ref="addFormRef" :model="addForm" :rules="addFromRules" label-width="80px">
+        <el-form-item label="用户名" prop="username">
+          <el-input v-model="addForm.username"></el-input>
+        </el-form-item>
+        <el-form-item label="密码" prop="password">
+          <el-input v-model="addForm.password"></el-input>
+        </el-form-item>
+        <el-form-item label="邮箱" prop="email">
+          <el-input v-model="addForm.email"></el-input>
+        </el-form-item>
+        <el-form-item label="手机" prop="mobile">
+          <el-input v-model="addForm.mobile"></el-input>
+        </el-form-item>
+      </el-form>
       <span slot="footer" class="dialog-footer">
         <el-button @click="addDialogVisible = false">取 消</el-button>
         <el-button type="primary" @click="addDialogVisible = false">确 定</el-button>
@@ -83,7 +96,33 @@ export default {
       },
       total: 0, // 总条数
       userList: [], // 用户列表
-      addDialogVisible: false // 控制添加用户弹框的显隐
+      addDialogVisible: false, // 控制添加用户弹框的显隐
+      // 添加用户的表单数据
+      addForm: {
+        username: '',
+        password: '',
+        email: '',
+        mobile: ''
+      },
+      // 添加用户的验证规则
+      addFromRules: {
+        username: [
+          { required: true, message: '请输入用户名', trigger: 'blur' },
+          { min: 3, max: 5, message: '长度在 3 到 10 个字符', trigger: 'blur' }
+        ],
+        password: [
+          { required: true, message: '请输入密码', trigger: 'blur' },
+          { min: 6, max: 15, message: '长度在 6 到 15 个字符', trigger: 'blur' }
+        ],
+        email: [
+          { required: true, message: '请输入邮箱', trigger: 'blur' },
+          // { validator: checkEmail, trigger: 'blur' }
+        ],
+        mobile: [
+          { required: true, message: '请输入手机号', trigger: 'blur' },
+          // { validator: checkMobile, trigger: 'blur' }
+        ],
+      },
     }
   },
 
