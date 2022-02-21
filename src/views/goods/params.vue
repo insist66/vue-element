ab250a10c44e58f5bceb8898c4b5f8b9ece81448
sbobo <sbobo@laixue365.com>
2022-02-21 23:06:30 +0800

    分类参数的列表与添加

--- src/views/goods/params.vue
+++ src/views/goods/params.vue
@@ -23,15 +23,63 @@
         </el-col>
       </el-row>
 
+      <!-- 表格区域 -->
       <el-tabs v-model="activeName" @tab-click="handleTabClick">
         <el-tab-pane label="动态参数" name="many">
-          <el-button type="primary" size="mini" :disabled="isBtnDisabled">添加参数</el-button>
+          <el-button type="primary" size="mini" :disabled="isBtnDisabled" @click="addDialogVisible = true">添加参数</el-button>
+          <!-- 动态参数列表 -->
+          <el-table :data="manyTableData" style="width: 100%" border stripe> 
+            <el-table-column type="expand"></el-table-column>
+            <!-- 参数名称 -->
+            <el-table-column type="index" label="#"></el-table-column>
+            <el-table-column prop="attr_name" label="参数名称"></el-table-column>
+            <el-table-column label="操作">
+              <template v-slot="scope">
+                <!-- 修改按钮 -->
+                <el-button type="primary" icon="el-icon-edit" size="mini" @click="showEditDialog(scope.row.id)"></el-button>
+                <!-- 删除按钮 -->
+                <el-button type="danger" icon="el-icon-delete" size="mini" @click="removeUserById(scope.row.id)"></el-button>
+              </template>
+            </el-table-column>
+          </el-table>
         </el-tab-pane>
+
         <el-tab-pane label="静态参数" name="only">
-          <el-button type="primary" size="mini" :disabled="isBtnDisabled">静态参数</el-button>
+          <el-button type="primary" size="mini" :disabled="isBtnDisabled" @click="addDialogVisible = true">添加属性</el-button>
+          <el-table :data="onlyTableData" style="width: 100%" border stripe> 
+            <el-table-column type="expand"></el-table-column>
+            <!-- 属性名称 -->
+            <el-table-column type="index" label="#"></el-table-column>
+            <el-table-column prop="attr_name" label="参数名称"></el-table-column>
+            <el-table-column label="操作">
+              <template v-slot="scope">
+                <!-- 修改按钮 -->
+                <el-button type="primary" icon="el-icon-edit" size="mini" @click="showEditDialog(scope.row.id)"></el-button>
+                <!-- 删除按钮 -->
+                <el-button type="danger" icon="el-icon-delete" size="mini" @click="removeUserById(scope.row.id)"></el-button>
+              </template>
+            </el-table-column>
+          </el-table>
         </el-tab-pane>
       </el-tabs>
     </el-card>
+
+    <el-dialog
+      :title="'添加' + textTitle"
+      :visible.sync="addDialogVisible"
+      width="50%"
+      :before-close="handleClose"
+      @close="addDialogClose">
+      <el-form :model="addRuleForm" :rules="addRules" ref="addFormRef" label-width="100px">
+        <el-form-item :label="textTitle" prop="attr_name">
+          <el-input v-model="addRuleForm.attr_name"></el-input>
+        </el-form-item>
+      </el-form>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="addDialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="addParams">确 定</el-button>
+      </span>
+    </el-dialog>
   </div>
 </template>
 
@@ -46,7 +94,20 @@ export default {
         children: 'children'
       },
       seletedKeys: [],
-      activeName: 'many', // 
+      activeName: 'many', // 默认激活为动态参数
+      manyTableData: [], // 动态参数数组
+      onlyTableData: [], // 静态参数数组
+      addDialogVisible: false, // 控制添加参数弹框显隐
+      addRuleForm: { // 添加属性参数的数据对象
+        attr_name: ''
+      },
+      // 添加动态参数静态属性的字段校验
+      addRules: {
+        attr_name: [
+          { required: true, message: '请输入活动名称', trigger: 'blur' },
+          { min: 3, max: 10, message: '长度在 3 到 10 个字符', trigger: 'blur' }
+        ]
+      }
     }
   },
 
@@ -69,8 +130,15 @@ export default {
         return this.seletedKeys[2]
       }
       return null
-    }
-  },
+    },
+
+    textTitle() {
+      if(this.activeName == 'many') {
+        return '动态参数'
+      }
+      return '静态属性'
+    },
+  }, 
 
   methods: {
     // 商品分类列表
@@ -93,10 +161,33 @@ export default {
       const {data: res} = await this.$http.get(`categories/${this.cateId}/attributes`, {
         params: {sel: this.activeName}
       })
+      console.log(res);
       if(res.meta.status !== 200) {
         return this.$message.error('获取参数列表失败')
       }
-      console.log(res);
+      
+      // 判断是静态参数还是动态参数 分别赋值
+      if(this.activeName == 'many') {
+        this.manyTableData = res.data;
+      } else {
+        this.onlyTableData = res.data;
+      }
+    },
+
+    addParams() {
+      this.$refs.addFormRef.validate(async valid => {
+        if(!valid) return
+        const { data: res } = await this.$http.post(`categories/${this.cateId}/attributes`, {
+          attr_name: this.addRuleForm.attr_name,
+          attr_sel: this.activeName
+        })
+        if (res.meta.status !== 201) {
+          return this.$message.error('添加属性失败')
+        }
+        this.$message.success('添加属性成功');
+        this.getParamsData();
+        this.addDialogVisible = false;
+      })
     },
 
     // 只允许选择三级分类
@@ -107,6 +198,16 @@ export default {
     handleTabClick() {
       console.log(this.activeName);
       this.getParamsData();
+    },
+
+    // 监听添加用户对话框额关闭事件
+    addDialogClose() {
+      this.$refs.addFormRef.resetFields();
+    },
+
+    // 关闭弹框
+    handleClose() {
+      this.addDialogVisible = false;
     }
   }
 }
